b45d1cf1dd4f55b4e7207f6a145f1cba069a6a1c
Philip <ficklephil@hotmail.com>
2013-10-06 21:54:33 +0100

    Map on page moves to lat lng!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,4 +15,15 @@ angular.module('thenhomeUiApp')
     $scope.markers = [];
     $scope.zoom = 8;
 
+    $scope.beanLand = function(){
+        console.log('ng beanland clicked');
+
+        $scope.center = {
+            latitude: 51.461035, // initial map center latitude
+            longitude: -0.352957 // initial map center longitude
+        }
+        $scope.markers = [];
+        $scope.zoom = 8;
+    }
+
   });
